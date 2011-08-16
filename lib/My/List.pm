bbbb4f0ae79cf883b5afedcd7249c0090e5f79d9
OKU_K <oku.87.s62.a4@gmail.com>
2011-08-16 14:32:50 +0900

    submit MyList

---
+++ lib/My/List.pm
@@ -0,0 +1,52 @@
+package My::List;
+
+{ package Iterator;
+  sub new {
+      my $class = shift;
+      my $self = { List => shift };
+      bless $self, $class;
+  }
+  sub has_next {
+      my $self = shift;
+      my $list = $self->{List};
+      ${$list}->{Next};
+  }
+  sub next {
+      my $self = shift;
+      my $list = $self->{List};
+      $self->{List} = ${$list}->{Next};
+      $self->{List};
+  }
+}
+
+sub new {
+    my $class = shift;
+    my $self = { Value => undef, Next => undef };
+    bless \$self, $class;
+}
+
+sub iterator {
+    my $self = shift;
+    Iterator->new($self);
+}
+
+
+sub append {
+    my $self = shift;
+    my $new = My::List->new;
+    ${$new}->{Value} = shift;
+    
+    while (${$self}->{Next}) {
+	$self = ${$self}->{Next};
+    }
+    
+    ${$self}->{Next} = $new;
+}
+
+sub value {
+    my $self = shift;
+    ${$self}->{Value};
+}
+
+
+1;
---
+++ t/test.t
@@ -0,0 +1,17 @@
+use My::List;
+
+
+
+my $list = My::List->new;
+
+$list->append("Hello");
+$list->append("World");
+$list->append(2011);
+
+
+my $iter = $list->iterator;
+
+
+while ($iter->has_next) {
+    print $iter->next->value;
+}
